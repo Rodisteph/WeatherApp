f2ae1e846861bd6cee5f7a4630fe63a4937c4df1
Rodrigo Bouabida <rdrgbouabida@gmail.com>
2026-05-06 17:37:18 +0200

    Refactor fetchWeather method and update comments

--- ViewModels/WeatherViewModel.swift
+++ ViewModels/WeatherViewModel.swift
@@ -6,8 +6,7 @@ import Combine
 
 @MainActor
 class WeatherViewModel: ObservableObject {
-    
-    // 📡 Les données que la View va afficher
+
     @Published var weather: WeatherResponse?
     @Published var errorMessage: String?
     @Published var isLoading: Bool = false
@@ -16,21 +15,21 @@ class WeatherViewModel: ObservableObject {
     
     func fetchWeather(for city: String) async {
         
-        // 1. On démarre le chargement
+       
         isLoading = true
         errorMessage = nil
         
         do {
-            // 2. On appelle le service
+            
             weather = try await service.fetchWeather(for: city)
             
         } catch {
-            // 3. Si erreur → on affiche un message
+            
             errorMessage = "Ville introuvable 😕"
             weather = nil
         }
         
-        // 4. Chargement terminé
+    
         isLoading = false
     }
 }
